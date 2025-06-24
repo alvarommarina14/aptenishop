616d56a49e13f736c2dbe46cb12089389e180e1d
Galo <durantegalo@gmail.com>
2025-06-24 16:11:36 -0300

    Create simple product page layout

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,8 +1,37 @@
-export default function Product() {
+import { GetProductById } from "@/lib/services/products";
+
+type Props = {
+  params: { id: string };
+};
+
+export default async function Product({ params }: Props) {
+  const product = await GetProductById(parseInt(params.id));
+
+  if (!product) {
+    return <p>Producto no encontrado</p>;
+  }
+
+  const selectedVariant = product.variants.find((v) => v.isAvailable) || product.variants[0];
+
   return (
     <main>
       <div>
-        <h1>Product Page</h1>
+        <div className="flex">
+          <div>
+            <img src={selectedVariant.images[0].url} alt={selectedVariant.images[0].altText ?? undefined} />
+          </div>
+          <div>
+            <p className="font-semibold">{product.brand}</p>
+            <h1>{product.name}</h1>
+            <div className="flex items-center gap-2">
+              <p className="font-semibold text-xl">${selectedVariant.price}</p>
+              {selectedVariant.compareAtPrice && selectedVariant.compareAtPrice > selectedVariant.price && (
+                <p className="line-through text-lg text-gray-400">${selectedVariant.compareAtPrice}</p>
+              )}
+            </div>
+            {!selectedVariant.isAvailable && <p>No stock available</p>}
+          </div>
+        </div>
       </div>
     </main>
   );
